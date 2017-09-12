94b9f8e7d72a085490a323b9260a9d3889b93320
xander <xander.liu@outlook.com>
2017-09-12 20:05:29 +0800

    添加一些文件

---
+++ src/git/test2.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package git;
+
+import java.awt.HeadlessException;
+
+/**
+ * @author xander
+ * @Data 2017年9月12日下午5:37:39
+ * @Description:
+ */
+public class test2 {
+
+    /**
+     * @Description:
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Hello Git !");
+
+    }
+
+}
